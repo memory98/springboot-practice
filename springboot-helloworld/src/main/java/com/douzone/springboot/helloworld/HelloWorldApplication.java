2680640d01fa43fd9c933e3d58ae8db42df95101
memory98 <asdlkjh252567@gmail.com>
2023-02-15 15:54:59 +0900

    springboot-helloworld

--- springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.douzone.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 
@@ -24,6 +25,20 @@ public class HelloWorldApplication {
 		 * 	  - 서버 인스턴스에 웹 애플리케이션 배포
 		 * 5. ApplicationRunner 인터페이스를 구현한 빈을 Application Context에서 찾아서 실행
 		 */
-		SpringApplication.run(HelloWorldApplication.class, args);
+//		ConfigurableApplicationContext ac = null;
+//		try {
+//			ac = SpringApplication.run(HelloWorldApplication.class, args);
+//		} catch(Throwable e) {
+//			e.printStackTrace();
+//		} finally {
+//			if(ac != null) {
+//				ac.close();		
+//			}
+//		}
+		
+		// try ~ with ~ resources 구문
+		try(ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)) {
+			
+		}
 	}
 }
