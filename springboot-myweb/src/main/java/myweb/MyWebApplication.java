50fb34e2546dd03c18f39e8f22d850325d9730e3
memory98 <asdlkjh252567@gmail.com>
2023-02-20 10:09:28 +0900

    .

--- springboot-myweb/src/main/java/myweb/MyWebApplication.java
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MyWebApplication {
-	
+	@ResponseBody
 	@Controller
 	public class HelloController {
 		
